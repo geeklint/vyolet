c07a58c5dafc5ed45404e2acfc55cc6d77c18e71
geeklint <geeklint@geeklint.com>
2013-12-03 20:59:28 -0500

    test hook

--- world_gen.py
+++ world_gen.py
@@ -15,6 +15,7 @@
     You should have received a copy of the GNU General Public License
     along with Vyolet.  If not, see <http://www.gnu.org/licenses/>.
 '''
+
 import random
 
 import spaceobjects
