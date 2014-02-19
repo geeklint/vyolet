0bdcae4d817fd162048ade351bed02669adc5095
geeklint <geeklint@geeklint.com>
2014-02-19 15:21:45 -0500

    Work on cargo

--- vyolet/modalviews/cargo.py
+++ vyolet/modalviews/cargo.py
@@ -15,12 +15,19 @@ This file is part of Vyolet.
     along with Vyolet.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
+import pygame
+
 from . import ModalView
 
 class Cargo(ModalView):
     def __init__(self, gp):
         super(Cargo, self).__init__(gp)
         self.tab = 0
+        self.scroll = 0
 
     def draw(self, screen, size):
-        pass
+        screen.fill((0x80, 0x80, 0x80))
+        area = pygame.Rect(0, 30 * self.tab, 200, 30)
+        screen.blit(self.gp.png.cargotabs, (0, 0), area)
+        screen = screen.subsurface((0, 30, size[0], size[1] - 30))
+
